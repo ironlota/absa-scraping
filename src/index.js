1fe209c6009628128a3e06be1d96db0d6cedec51
Ray Andrew <raydreww@gmail.com>
2018-10-30 10:56:14 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+const program = require('commander');
+// const chalk = require('chalk');
+const _ = require('lodash');
+
+const pkg = require('../package.json');
+
+const scrape = require('./scripts/raw_scraping');
+
+const range = (val) => val.split('..').map(Number);
+
+program
+  .version(pkg.version)
+  .option('-s, --scrape <a>..<b>', 'Scrape Data', range)
+  // .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .parse(process.argv);
+
+program.scrape = program.scrape || [];
+
+if (!_.isEmpty(program.scrape)) {
+  Promise.resolve(scrape('./data', program.scrape[0], program.scrape[1]));
+}
+// console.log('  - %s cheese', program.cheese);
---
+++ src/scripts/raw_scraping.js
@@ -0,0 +1,64 @@
+const Promise = require('bluebird');
+const axios = require('axios');
+const _ = require('lodash');
+
+const chalk = require('chalk');
+const ora = require('ora');
+
+const getter = require('../utils/exception-free');
+const fs = require('../utils/fs-promise');
+
+const spinnerAllChunks = ora(chalk.red(`${chalk.underline.bgBlue('SCRAPING')} Data!\n`)).start();
+
+// 1137100  axios(`https://www.trivago.co.id/api/v1/_cache/accommodation/${value}/ratings.json?requestId=v12_10_5_ab_id_ID_UK`)
+
+const scrapeTrivago = async (chunk) => {
+  const promises = chunk
+    .map(value =>
+      axios.get(`https://www.trivago.co.id/api/v1/_cache/accommodation/${value}/ratings.json?requestId=v12_10_5_ab_id_ID_UK`)
+        .then(async result => [value, result.data])
+        .catch(() => [value, false])
+    );
+
+  try {
+    return await Promise.all(promises);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const scrapingAction = async (folder, chunk) => {
+  try {
+    const res = await scrapeTrivago(chunk);
+    return res.map(([id, value]) => {
+      const reviews = getter(value.hydraReviews.reviews);
+      const selectedLang = getter(value.hydraReviewsselectedLanguage);
+
+      if (!value || _.isEmpty(reviews) || selectedLang !== 'en') return Promise.resolve(false);
+
+      const normalizedReview = value.hydraReviews.reviews.map((review) => review.text);
+
+      return fs.writeFileAsync(`./${folder}/${id}.json`, JSON.stringify(normalizedReview, null, 2), 'utf8');
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = async (folder, minRange, maxRange) => {
+  try {
+    const isDirCreated = await fs.existsAsync(folder);
+    if (!isDirCreated) await fs.mkdirAsync(folder);
+
+    await _.chunk(_.range(minRange, maxRange, 2), 10)
+      .reduce((promise, chunk) => promise.then(() => {
+        console.log(chalk.red(`${chalk.underline.bgBlue('SCRAPING')} chunks no: ${chunk} \n`));
+
+        return Promise.all([Promise.delay(2000), scrapingAction(folder, chunk)]);
+      }), Promise.resolve());
+
+    spinnerAllChunks.succeed(chalk.green(`${chalk.underline.bgBlue('SCRAPING')} Done!`));
+  } catch (e) {
+    console.log(e);
+  }
+};
